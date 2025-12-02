e0cc6f61d1792f4ca0653ab743cbd6d27946b963
Nathan <nathannmvr@gmail.com>
2025-12-02 19:47:38 -0300

    refactor: adaptar TudoMinuscula para usar ConversorTexto

--- TudoMinuscula.java
+++ TudoMinuscula.java
@@ -1,23 +1,15 @@
-public class TudoMinuscula {
-    public static String converterTextoEmMinusculo(String entrada) {
-        char[] caracteres = extrairCaracteres(entrada);
-        char[] caracateresAgoraMinusculos = converterCaracteresEmMinusculo(caracteres);
-        String resultado = juntarCaractaresAgoraMinusculo(caracateresAgoraMinusculos);
+public class TudoMinuscula extends ConversorTexto {
+    public String converterTextoEmMinusculo(String entrada) {
+        return converterTexto(entrada);
+    }
+
+    @Override
+    protected char[] transformarCaracteres(char[] chars) {
+        if (chars == null) return new char[0];
+        char[] resultado = new char[chars.length];
+        for (int i = 0; i < chars.length; i++) {
+            resultado[i] = Character.toLowerCase(chars[i]);
+        }
         return resultado;
-    }
-
-    private static String juntarCaractaresAgoraMinusculo(char[] chars) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    private static char[] converterCaracteresEmMinusculo(char[] chars) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    private static char[] extrairCaracteres(String texto) {
-        // TODO Auto-generated method stub
-        return null;
     }
 }
